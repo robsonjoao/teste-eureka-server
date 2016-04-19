c17ec3e5c431b36922dea422762d2174a700b3d2
Robson Joao Padilha da Luz <robson.luz@bomjesus.br>
2016-04-19 13:17:04 -0300

    Ajustes autoconf

--- src/main/java/edu/bj/reservation/eureka/TesteEurekaServerApplication.java
+++ src/main/java/edu/bj/reservation/eureka/TesteEurekaServerApplication.java
@@ -1,12 +1,16 @@
 package edu.bj.reservation.eureka;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableAutoConfiguration
+@Controller
 public class TesteEurekaServerApplication {
 
 	public static void main(String[] args) {
