66d3aef15a50b6e473fef975a90dd82c428d68a9
Robson Joao Padilha da Luz <robson.luz@bomjesus.br>
2016-04-19 13:02:31 -0300

    Servico de teste

--- src/main/java/edu/bj/reservation/eureka/TesteEurekaServerApplication.java
+++ src/main/java/edu/bj/reservation/eureka/TesteEurekaServerApplication.java
@@ -3,6 +3,7 @@ package edu.bj.reservation.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -11,4 +12,10 @@ public class TesteEurekaServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TesteEurekaServerApplication.class, args);
 	}
+	
+	
+	@RequestMapping("/messagex")
+	String message() {
+		return "tatata";
+	}	
 }
