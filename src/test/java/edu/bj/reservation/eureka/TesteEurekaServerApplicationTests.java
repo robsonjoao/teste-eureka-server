e88e07260d2c2901ffc685529927437a03056590
Robson Joao Padilha da Luz <robson.luz@bomjesus.br>
2016-04-19 12:48:26 -0300

    Projeto inicial

---
+++ src/test/java/edu/bj/reservation/eureka/TesteEurekaServerApplicationTests.java
@@ -0,0 +1,16 @@
+package edu.bj.reservation.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TesteEurekaServerApplication.class)
+public class TesteEurekaServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
